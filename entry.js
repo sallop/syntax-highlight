7de5bb8a47a5e1a1a9e621d52be3bf794454840c
sallop <shiogai1987@yahoo.co.jp>
2016-07-06 19:54:35 +0900

    delete unnecessary lines

--- entry.js
+++ entry.js
@@ -1,7 +1,8 @@
 //var h1 = require('highlight').Highlight;
 
 require('./style.css');
-require('./node_modules/highlight.js/styles/default.css');
+//require('./node_modules/highlight.js/styles/default.css');
+//require('./node_modules/highlight.js/styles/rainbow.css');
 
 var hljs = require('highlight.js');
 //var hljs = require("./node_modules/highlight.js/lib/highlight.js");
